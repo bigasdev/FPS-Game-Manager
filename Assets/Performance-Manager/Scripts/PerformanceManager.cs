1c5475e4b9ad1ef6efa06b4eb8a38ed937ca20f3
gustavo domingues <deathdiec@gmail.com>
2022-09-03 23:02:15 -0300

    started the GC part + exported the package

--- Assets/Performance-Manager/Scripts/PerformanceManager.cs
+++ Assets/Performance-Manager/Scripts/PerformanceManager.cs
@@ -9,17 +9,35 @@ namespace Performance{
         [Header("FPS")]
         [SerializeField] int maxFps = 60;
         [SerializeField] Text fpsText;
-        public bool isWorking = true;
         float deltaTime;
+
+        [Header("GC")]
+        [SerializeField] Text gcText;
+
+        [Header("Variables")]
+        public bool isWorking = true;
+        public float tickTimer = .125f;
+        float tick = 0;
         void Update () {
             if(!isWorking)return;
             CheckFps();
+            TickTimer();
+        }
+        void TickTimer(){
+            tick += Time.deltaTime;
+            while(tick >= tickTimer){
+                tick -= tickTimer;
+                CheckGC();
+            }
         }
         void CheckFps(){
             deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
             float fps = 1.0f / deltaTime;
             var f = Mathf.Clamp(fps, 0, maxFps);
             fpsText.text = $"FPS - {Mathf.Ceil (f).ToString ()}";
+        }
+        void CheckGC(){
+            gcText.text = $"GC - {string.Format("{0:0.##}", System.GC.GetTotalMemory(false)/1024)}";
         }
     }
 }
